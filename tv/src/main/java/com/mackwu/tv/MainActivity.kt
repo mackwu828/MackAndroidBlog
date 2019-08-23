923af40772ca212a86f0bceb3cba99824e6bddaa
mack.wu <mack.wu@zeasn.com>
2019-08-23 18:50:13 +0800

    first commit

---
+++ tv/src/main/java/com/mackwu/tv/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mackwu.tv
+
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
