feb26c4848fb9e538330d94f1ef808faf3707b6d
mack.wu <mack.wu@zeasn.com>
2019-08-27 15:27:46 +0800

    update http xml

---
+++ http/src/main/java/com/mackwu/http/xml/UserBean.kt
@@ -0,0 +1,15 @@
+package com.mackwu.http.xml
+
+/**
+ * ================================================
+ * Created by MackWu on 2019/8/23 19:30
+ * <a href="mailto:wumengjiao828@163.com">Contact me</a>
+ * <a href="https://github.com/mackwu828">Follow me</a>
+ * ================================================
+ */
+data class UserBean (
+        var id: String = "",
+        var name: String = "",
+        var password: String = "",
+        var age: String = ""
+)
