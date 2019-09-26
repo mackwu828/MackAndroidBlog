2f3bde590800c2d466b9bb7684f2f0c2e2d0fbf1
mack.wu <mack.wu@zeasn.com>
2019-09-26 17:18:28 +0800

    add util

---
+++ util/src/main/java/com/mackwu/util/NetworkExt.kt
@@ -0,0 +1,48 @@
+package com.mackwu.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+
+/**
+ * ===================================================
+ * Created by MackWu on 2019/7/26 15:44
+ * <a href="mailto:wumengjiao828@163.com">Contact me</a>
+ * <a href="https://github.com/mackwu828">Follow me</a>
+ * ===================================================
+ */
+private val Context.connectivityManager
+    get() = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+private val Context.networkInfo: NetworkInfo?
+    get() = connectivityManager.activeNetworkInfo
+
+/**
+ * 检查是否有网络
+ */
+val Context.isNetworkAvailable: Boolean
+    get() = null != networkInfo && networkInfo!!.isAvailable
+
+/**
+ * 检查是否是WIFI
+ */
+val Context.isWifi: Boolean
+    get() = null != networkInfo && networkInfo!!.type == ConnectivityManager.TYPE_WIFI
+
+/**
+ * 检查是否是移动网络
+ */
+val Context.isMobile: Boolean
+    get() = null != networkInfo && networkInfo!!.type == ConnectivityManager.TYPE_MOBILE
+
+
+/**
+ * 监听是否有网络
+ */
+//fun Context.requestNetworkAvailable(listener: () -> Unit){
+//    connectivityManager.requestNetwork(NetworkRequest.Builder().build(), object : ConnectivityManager.NetworkCallback(){
+//        override fun onAvailable(network: Network?) {
+//            logD("requestNetworkAvailable: onAvailable")
+//            listener.invoke()
+//        }
+//    })
+//}
---
+++ util/src/main/java/com/mackwu/util/SpExt.kt
@@ -0,0 +1,91 @@
+package com.mackwu.util
+
+import android.content.Context
+import android.support.v4.app.Fragment
+
+/**
+ * ===================================================
+ * Created by MackWu on 2019/9/25 14:21
+ * <a href="mailto:wumengjiao828@163.com">Contact me</a>
+ * <a href="https://github.com/mackwu828">Follow me</a>
+ * ===================================================
+ */
+private const val SP_FILE_NAME = "share_data"
+
+/**
+ * 保存数据
+ */
+fun Context.put(key: String, obj: Any) {
+    val sp = getSharedPreferences(SP_FILE_NAME, Context.MODE_PRIVATE)
+    val editor = sp.edit()
+    when (obj) {
+        is Boolean -> editor.putBoolean(key, obj)
+        is Float -> editor.putFloat(key, obj)
+        is Int -> editor.putInt(key, obj)
+        is Long -> editor.putLong(key, obj)
+        else -> editor.putString(key, obj as String)
+    }
+    editor.commit()
+    //        editor.apply();
+}
+
+
+/**
+ * 获取数据
+ */
+fun Context.get(key: String, defaultObj: Any): Any? {
+    val sp = getSharedPreferences(SP_FILE_NAME, Context.MODE_PRIVATE)
+    return when (defaultObj) {
+        is Boolean -> sp.getBoolean(key, defaultObj)
+        is Float -> sp.getFloat(key, defaultObj)
+        is Int -> sp.getInt(key, defaultObj)
+        is Long -> sp.getLong(key, defaultObj)
+        is String -> sp.getString(key, defaultObj)
+        else -> null
+    }
+}
+
+fun Fragment.put(key: String, obj: Any) {
+    activity?.put(key, obj)
+}
+
+fun Fragment.get(key: String, defaultObj: Any) : Any? {
+    return activity?.get(key, defaultObj)
+}
+
+///**
+// * 删除指定数据
+// */
+//fun Context.remove(context: Context, key: String) {
+//    val sp = context.getSharedPreferences(SP_FILE_NAME, Context.MODE_PRIVATE)
+//    val editor = sp.edit()
+//    editor.remove(key)
+//    editor.commit()
+//}
+//
+//
+///**
+// * 返回所有键值对
+// */
+//fun Context.getAll(context: Context): Map<String, *> {
+//    val sp = context.getSharedPreferences(SP_FILE_NAME, Context.MODE_PRIVATE)
+//    return sp.all
+//}
+//
+///**
+// * 删除所有数据
+// */
+//fun Context.clear(context: Context) {
+//    val sp = context.getSharedPreferences(SP_FILE_NAME, Context.MODE_PRIVATE)
+//    val editor = sp.edit()
+//    editor.clear()
+//    editor.commit()
+//}
+//
+///**
+// * 检查key对应的数据是否存在
+// */
+//fun Context.contains(context: Context, key: String): Boolean {
+//    val sp = context.getSharedPreferences(SP_FILE_NAME, Context.MODE_PRIVATE)
+//    return sp.contains(key)
+//}
